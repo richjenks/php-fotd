08be51ad1f40a6f774b9bd26f02fdc3ec0c6418e
Rich Jenks <rich@richjenks.com>
2015-08-19 14:13:41 +0100

    Updated GA property ID

--- index.php
+++ index.php
@@ -145,19 +145,13 @@ if (!$already_chosen) {
 		<hr>
 		<p class="faded">Why did I make this? PHP currently has more than 5,000 functions, of which you probably only use couple of hundred regularly. Taking inspiration from the idea behind "Learn a Word a Day", PHP <abbr title="Function of the Day">FOTD</abbr> selects a new PHP function every day so 5,000 isn't so intimidating!</p>
 	</div>
-	<?php
 
-	// if (file_exists('../../misc/ga/ga.js')) {
-	// 	echo '<script src="../../misc/ga/ga.js"></script>';
-	// }
-
-	?>
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-48459309-5', 'auto');
+		ga('create', 'UA-48459309-1', 'auto');
 		ga('send', 'pageview');
 	</script>
 </body>
